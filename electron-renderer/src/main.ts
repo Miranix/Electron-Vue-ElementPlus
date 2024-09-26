9eba17c25cf6d00d25d8f675f4451c79114e1d91
Daimi <wzt252951@163.com>
2024-09-26 08:09:39 +0800

    add title

--- electron-renderer/src/main.ts
+++ electron-renderer/src/main.ts
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
+import packageJson  from '../../package.json'
 
+document.title = packageJson.name;
 const app = createApp(App)
 
 app.use(ElementPlus)
